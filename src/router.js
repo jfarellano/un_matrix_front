37c0eda96020a656b67b16fe8efe1bbc404d86cc
Juan Arellano <jfarellano@uninorte.edu.co>
2019-07-31 20:47:42 -0500

    App component created and max with controlled

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Login from './views/login.vue'
+import App from './views/app.vue'
 Vue.use(Router)
 export default new Router ({
     routes: [
@@ -9,5 +10,10 @@ export default new Router ({
             name: 'Login',
             component: Login
         },
+        {
+            path: '/app',
+            name: 'App',
+            component: App
+        }
     ]
 })
---
+++ src/views/app.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+section
+	Header
+	section.container-fluid.main
+		div.row
+			div.col
+				div.content
+	
+</template>
+
+<script>
+import Header from '../components/header.vue'
+export default {
+	components: {
+		Header
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+.main{
+	margin-top: 90px;
+	.col{
+		background-color: #edf5e1;
+	}
+	.content{
+		@media (min-width: 1100px){
+			width: 1100px;
+			margin: auto;
+		}
+		min-height: calc(100vh - 90px);
+		width: 100%;
+		background-color: white;
+	}
+}
+</style>
+
