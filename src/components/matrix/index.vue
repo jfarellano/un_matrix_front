85def7f20dcd6e2f4624501a2305b21a47f398df
Juan Arellano <jfarellano@uninorte.edu.co>
2019-08-03 10:13:18 -0500

    After matrix create generate link

--- src/components/matrix/index.vue
+++ src/components/matrix/index.vue
@@ -1,11 +1,19 @@
 <template lang="pug">
 section.container-fluid
 	vue-snotify
+	b-modal#showMatrix(title='Nueva matriz de conflicto', hide-footer='', hide-header)
+		div.modal-container.container-fluid
+			div.row
+				div.col
+					h4 Nombre de la matriz
+					input(v-model='matrix.name')
+					b-button(@click="addMatrix()") Guardar
+	b-button.add-new(@click='showMatrix()') Agregar nueva
 	div.row
 		div.col
-			b-button-group.list-item(v-for='matrix in matrixes' :key='matrix.id')
-				router-link.info(tag='b-button', to='habla') {{matrix.name}}
-				b-button.option(@click="deleteM(matrix.id)")
+			b-button-group.list-item(v-for='mat in matrixes' :key='mat.id')
+				router-link.info(tag='b-button', :to='"/app/matrix/" + mat.id') {{mat.name}}
+				b-button.option(@click="deleteM(mat.id)")
 					font-awesome-icon(icon='trash')
 </template>
 
@@ -14,7 +22,8 @@ import api from "../../requests";
 export default {
   data() {
     return {
-      matrixes: []
+      matrixes: [],
+      matrix: {}
     };
   },
   methods: {
@@ -25,54 +34,95 @@ export default {
           this.matrixes = response.data;
         })
         .catch(() => {
-          this.$snotify.error(
-            "Algo no salio bien, intenta de nuevo",
-            "Error"
-          );
+          this.$snotify.error("Algo no salio bien, intenta de nuevo", "Error");
         });
-		},
-		deleteM(id) {
-			console.log('Hola')
-			api.requests.matrix.delete(id).then(()=>{
-				this.getMatrixes()
-				this.$snotify.error(
-            "Se borro correctamente",
-            "Exito"
-          );
-			}).catch(() => {
-				this.$snotify.error(
-            "Algo no salio bien, intenta de nuevo",
-            "Error"
-          );
-			})
-		}
-	},
-	created(){
-		this.getMatrixes()
-	}
+    },
+    deleteM(id) {
+      api.requests.matrix
+        .delete(id)
+        .then(() => {
+          this.getMatrixes();
+          this.$snotify.error("Se borro correctamente", "Exito");
+        })
+        .catch(() => {
+          this.$snotify.error("Algo no salio bien, intenta de nuevo", "Error");
+        });
+    },
+    showMatrix() {
+      this.$bvModal.show("showMatrix");
+    },
+    addMatrix() {
+      api.requests.matrix
+        .create(this.matrix)
+        .then((response) => {
+					this.getMatrixes();
+					api.requests.matrix.createLink(response.data.id)
+          this.matrix = {};
+          this.$bvModal.hide("showMatrix");
+        })
+        .catch(() => {
+          this.$snotify.error("Algo no salio bien, intenta de nuevo", "Error");
+        });
+    }
+  },
+  created() {
+    this.getMatrixes();
+  }
 };
 </script>
 
 <style lang="scss" scoped>
-.list-item {
-  width: 100%;
-  border: 1px solid #5cdb95;
-  border-radius: 5px;
-  color: #05386b;
-  margin-top: 7px;
-  .info {
-    width: 100%;
-    background-color: transparent;
-    color: #05386b;
-    flex: 0 1 auto;
-    text-align: left;
-    border: none;
+.modal {
+  .modal-body {
+		text-align: center;
+    h4 {
+      color: #05386b;
+    }
+    input {
+      width: 100%;
+      color: #05386b;
+      border: 1px solid #5cdb95;
+      margin: 5px;
+			border-radius: 5px;
+    }
+    button {
+      width: 100%;
+      background-color: #05386b;
+      color: white;
+      margin: 5px;
+    }
   }
-  .option {
-    width: 50px;
-    background-color: #c7161c;
-    color: white;
+}
+.container-fluid {
+  padding-top: 10px;
+  .add-new {
+    text-align: center;
+    height: 40px;
+    width: 100%;
     border: none;
+    background-color: #5cdb95;
+    color: #05386b;
+  }
+  .list-item {
+    width: 100%;
+    border: 1px solid #5cdb95;
+    border-radius: 5px;
+    color: #05386b;
+    margin-top: 7px;
+    .info {
+      width: 100%;
+      background-color: transparent;
+      color: #05386b;
+      flex: 0 1 auto;
+      text-align: left;
+      border: none;
+    }
+    .option {
+      width: 50px;
+      background-color: #c7161c;
+      color: white;
+      border: none;
+    }
   }
 }
 </style>
